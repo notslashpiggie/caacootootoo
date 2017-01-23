d7c14798bcaa52b2e174110284df2b3e17cf16c0
Binh Bui <baobinh.ams1013@gmail.com>
2017-01-23 17:08:42 +0200

    Initial Commit

---
+++ SaveTheDogeAndKillTheCate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SaveTheDogeAndKillTheCate
+//
+//  Created by Binh Bui on 23/01/2017.
+//  Copyright © 2017 Binh Bui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
